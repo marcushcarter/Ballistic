087a419992c0c57f9c1b3cf1ef595c05ef960715
Marcus Carter <marcoose408@gmail.com>
2025-09-04 10:15:58 -0400

    glad, glfw and window creation

---
+++ Makefile
@@ -0,0 +1,26 @@
+# === Configuration ===
+# === Compiler ===
+CXX := g++
+CXXFLAGS := -g -lm 
+
+# === Paths ===
+INCLUDES := -Iinclude -I.
+LDFLAGS := -Llib -lglfw3dll
+
+# === Files ===
+SRCS := src/main.cpp
+ENGINE_SCRS := $(wildcard include/*.cpp) $(wildcard include/glad/*.c)
+
+ALL_SRCS:= $(SRCS) $(ENGINE_SCRS)
+
+OUT := engine.exe
+
+# === Targets ===
+
+compile: test run
+
+test:
+	$(CXX) $(CXXFLAGS) $(INCLUDES) $(ALL_SRCS) -o $(OUT) $(LDFLAGS)
+
+run:
+	./$(OUT)
---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include "include/engine.hpp"
+
+#include <iostream>
+// #include <stdio.h>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    // printf("Hello, World!\n");
+
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 800, "Engine", NULL, NULL);
+    if (!window) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        exit(1);
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        exit(1);
+    }
+
+    while(!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    return 0;
+}
