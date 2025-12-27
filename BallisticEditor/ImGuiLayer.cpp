a24293600acef2cd4aaba15faee064b839364a9e
Marcus <marcus.carter@rogers.com>
2025-12-27 17:17:20 -0500

    Update the imgui submodul to docking branch

--- BallisticEditor/ImGuiLayer.cpp
+++ BallisticEditor/ImGuiLayer.cpp
@@ -2,8 +2,8 @@
 
 #include "imgui.h"
 #include "imgui_internal.h"
-#include "backends/imgui_impl_opengl3.h"
 #include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
 
 namespace ballistic
 {
@@ -13,9 +13,36 @@ namespace ballistic
     void ImGuiLayer::OnAttach() {
         IMGUI_CHECKVERSION();
         ImGui::CreateContext();
-        ImGuiIO& io = ImGui::GetIO(); (void)io;
+        ImGuiIO& io = ImGui::GetIO();
+	    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+	    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+	    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
 
-        ImGui::StyleColorsDark();
+        // ImFontConfig cfg;
+		// cfg.PixelSnapH = true;
+		// cfg.MergeMode = false;
+		// io.Fonts->AddFontFromFileTTF((Config::RESOURCES_PATH / "Fonts/Noto_Sans/static/NotoSans-Regular.ttf").string().c_str(), 16.0f, &cfg);
+		
+		// cfg.MergeMode = true;
+		// static const ImWchar icons_ranges[] = { 0xF000, 0xF8FF, 0 };
+		// io.Fonts->AddFontFromFileTTF((Config::RESOURCES_PATH / "Fonts/fontawesome-free-6.6.0-desktop/Font Awesome 6 Free-Solid-900.otf").string().c_str(), 13.0f, &cfg, icons_ranges);
+		
+    	ImGui::StyleColorsDark();
+
+        // ImGuiStyle& style = ImGui::GetStyle();
+        // if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+        //     // style.WindowRounding = 0.0f;
+        //     // style.TabRounding = 0.0f;
+        //     // style.TabBarBorderSize = 0.0f;
+        //     // style.GrabRounding = 2.0f;
+        //     // style.ScrollbarRounding = 2.0f;
+        //     // style.DockingSeparatorSize = 0.0f;
+        //     // style.WindowBorderSize = 0.0f;
+        // }
+
+        // style.WindowTitleAlign = ImVec2(0.5f, 0.5f);
+        // style.WindowMenuButtonPosition = ImGuiDir_None;
 
         ImGui_ImplGlfw_InitForOpenGL(static_cast<GLFWwindow*>(m_context.window->GetNativeWindow()), true);
         ImGui_ImplOpenGL3_Init("#version 460");
