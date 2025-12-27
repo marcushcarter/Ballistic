9e275e9163befe1bbf2dcd8bb5428d2e454ef4ca
Marcus <marcus.carter@rogers.com>
2025-12-27 10:39:30 -0500

    start of rewrite

---
+++ Ballistic/Ballistic.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "Core/app.h"
+
+#include "bepch.h"
---
+++ Ballistic/Entry/Entrypoint.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "bepch.h"
+
+int main(int argc, char** argv) {
+    std::cout << "Hello, World!\n";
+    while (true) {}
+    return 0;
+}
